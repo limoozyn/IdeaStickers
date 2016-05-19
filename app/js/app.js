26ad1c5cd6b74b9d27c3eda715d82d9710eb0343
Viacheslav <limoozyn@gmail.com>
2016-05-19 15:43:24 +0300

    Add another app.js

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,6 @@ var ideaSticksApp = angular.module('ideaSticksApp', [
   'ngRoute',
   'ideaSticksControllers'
 ]);
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000);
 ideaSticksApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
