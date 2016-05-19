f9c4bcb531617b0937cee5bfc98c913aa30b92b6
Viacheslav <limoozyn@gmail.com>
2016-05-19 13:07:41 +0300

    Add partials,routeProvider to serve them

---
+++ app/js/app.js
@@ -0,0 +1,21 @@
+var ideaSticksApp = angular.module('ideaSticksApp', [
+  'ngRoute',
+  'ideaSticksControllers'
+]);
+
+ideaSticksApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/types', {
+        templateUrl: 'partials/frontpage.html',
+        controller: 'whatWeDoCtrl'
+      }).
+      when('/types/:typeId', {
+        templateUrl: 'partials/type-detail.html',
+        controller: 'detailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/types'
+      });
+  }
+]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,15 @@
-var ideaSticksApp = angular.module('ideaSticksApp', []);
+var ideaSticksControllers = angular.module('ideaSticksControllers', []);
 
-ideaSticksApp.controller('whatWeDoCtrl',['$scope', '$http',
+ideaSticksControllers.controller('whatWeDoCtrl',['$scope', '$http',
     function ($scope, $http) {
       $http.get('types/types.json').success(function(data){
         $scope.types = data;
       })
+    }
+  ]);
+
+ideaSticksControllers.controller('detailCtrl',['$scope', '$routeParams',
+    function ($scope, $routeParams) {
+      $scope.typeId = $routeParams.typeId;
     }
   ]);
