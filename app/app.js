d0a37d7fc46d5f0f4198c3b957339523b27f3b31
Viacheslav <limoozyn@gmail.com>
2016-05-25 14:08:29 +0300

    Play with root route

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var express = require('express'),
 app = express();
 app.use(express.static(__dirname ));
 app.get('/', function(req, res) {
-    res.sendFile('index.html', {root: __dirname })
+    //res.sendFile('index.html', {root: __dirname })
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.listen(process.env.PORT || 3000);
