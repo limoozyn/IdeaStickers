4d6f7349e929b1ca7ab569fb64058e126390804b
Viacheslav <limoozyn@gmail.com>
2016-05-25 12:39:00 +0300

    Update app.js express dirname settings

--- app/app.js
+++ app/app.js
@@ -1,4 +1,7 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+app = express();
 app.use(express.static(__dirname + '/app'));
+app.get('/', function(req, res) {
+    res.sendfile('index.html', {root: __dirname + '/app' })
+});
 app.listen(process.env.PORT || 3000);
