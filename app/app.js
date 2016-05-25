de6034123c25a7998743c6440f3904f4848f57c9
Viacheslav <limoozyn@gmail.com>
2016-05-25 14:17:35 +0300

    Play with root route

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-app = express();
+app = express(),
+path = require('path');
 app.use(express.static(__dirname ));
 app.get('/', function(req, res) {
     //res.sendFile('index.html', {root: __dirname })
